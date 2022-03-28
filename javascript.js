c247a88d6030bc1d6a71fa1d837e97314ca90518
Aditi Vishwakarma <aditivishj@gmail.com>
2022-03-28 23:53:59 +0530

    1st

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// let hello = document.getElementById('newjs');
+
+// // hello = hello.className;
+// // hello = hello.childNodes
+// // hello = hello.parentNode;
+// // hello.style.color = 'red'
+// hello.innerText = 'imranpartydega'
+// hello.innerHTML = 'imranpartydega2bar'
+
+// console.log(hello);
+
+
+// let hello = document.querySelector('#newjs');
+
+// hello = document.querySelector('div');
+
+// // hello.style.color 
+
+// console.log(hello);
+
+// Array.from(hello).forEach(element => {
+//     console.log(element);
+//     element.style.color = 'yellow';
+
+// }
+    
+    
+//     )
